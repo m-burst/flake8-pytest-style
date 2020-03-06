ada917e6531d8d132967ecb87c54911c821da723
d1618033 <d1618033@gmail.com>
2020-03-06 18:36:28 +0200

    Use assert_error and assert_not_error from flake8-plugin-utils

--- tests/test_PT010_no_bare_raises.py
+++ tests/test_PT010_no_bare_raises.py
@@ -1,9 +1,4 @@
-import ast
-from textwrap import dedent
-from typing import Any, Optional, Type
-
-from flake8_plugin_utils import Visitor
-from flake8_plugin_utils.plugin import Error, TConfig
+from flake8_plugin_utils import assert_error, assert_not_error
 
 from flake8_pytest_style.config import Config
 from flake8_pytest_style.errors import NoBareRaises
@@ -54,39 +49,3 @@ def test_error_match_is_none():
                 raise ValueError("Can't divide 1 by 0")
     """
     assert_error(PytestStyleVisitor, code, NoBareRaises, config=CONFIG)
-
-
-def _error_from_src(
-    visitor_cls: Type[Visitor[TConfig]], src: str, config: Optional[TConfig] = None,
-) -> Optional[Error]:
-    visitor = visitor_cls(config=config)
-    tree = ast.parse(dedent(src))
-    visitor.visit(tree)
-    if not visitor.errors:
-        return None
-    assert len(visitor.errors) == 1
-    return visitor.errors[0]
-
-
-def assert_error(
-    visitor_cls: Type[Visitor[TConfig]],
-    src: str,
-    expected: Type[Error],
-    config: Optional[TConfig] = None,
-    **kwargs: Any,
-) -> None:
-    err = _error_from_src(visitor_cls, src, config=config)
-    assert err, f'Error "{expected.message}" not found in\n{src}'
-    assert isinstance(err, expected)
-
-    expected_message = expected.formatted_message(**kwargs)
-    assert (
-        expected_message == err.message
-    ), f'Expected error with message "{expected_message}", got "{err.message}"'
-
-
-def assert_not_error(
-    visitor_cls: Type[Visitor[Any]], src: str, config: Optional[TConfig] = None
-) -> None:
-    err = _error_from_src(visitor_cls, src, config=config)
-    assert not err, f'Error "{err.message}" found in\n{src}'  # type: ignore
