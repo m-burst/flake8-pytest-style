d9519fe1e1bf4fb94d2d9936b9263e4a3274bb57
Mikhail Burshteyn <mdburshteyn@gmail.com>
2025-01-10 18:47:05 +0400

    Refactor PT017 into separate visitor

--- tests/test_PT017_assert_in_except.py
+++ tests/test_PT017_assert_in_except.py
@@ -2,7 +2,7 @@ from flake8_plugin_utils.utils import assert_error, assert_not_error
 
 from flake8_pytest_style.config import DEFAULT_CONFIG
 from flake8_pytest_style.errors import AssertInExcept
-from flake8_pytest_style.visitors import RaisesVisitor
+from flake8_pytest_style.visitors import TryExceptVisitor
 
 
 def test_ok():
@@ -17,7 +17,7 @@ def test_ok():
                 1 / 0
             assert e.value.message
     """
-    assert_not_error(RaisesVisitor, code, config=DEFAULT_CONFIG)
+    assert_not_error(TryExceptVisitor, code, config=DEFAULT_CONFIG)
 
 
 def test_error():
@@ -28,4 +28,6 @@ def test_error():
             except Exception as e:
                 assert e.message, 'blah blah'
     """
-    assert_error(RaisesVisitor, code, AssertInExcept, name='e', config=DEFAULT_CONFIG)
+    assert_error(
+        TryExceptVisitor, code, AssertInExcept, name='e', config=DEFAULT_CONFIG
+    )
